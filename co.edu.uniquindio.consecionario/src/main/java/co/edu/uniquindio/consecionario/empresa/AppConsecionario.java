239383c1804a5176523f85874d0c918fde274131
AnduellgDeveloper <anduellgames@gmail.com>
2024-10-31 13:14:42 -0500

    Cambios

--- co.edu.uniquindio.consecionario/src/main/java/co/edu/uniquindio/consecionario/empresa/AppConsecionario.java
+++ co.edu.uniquindio.consecionario/src/main/java/co/edu/uniquindio/consecionario/empresa/AppConsecionario.java
@@ -10,44 +10,33 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AppConsecionario extends Application {
-
-    // Creamos una instancia de Dealer (el concesionario) que se compartirá entre los controladores.
     private Dealer dealer = new Dealer();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Cargar la ventana principal desde el archivo FXML
+        // Cargar la ventana principal del cliente
         FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("DealerView.fxml"));
         VBox mainRoot = mainLoader.load();
-
-        // Obtener el controlador de la ventana principal y configurar el concesionario
         ConsecionarioController mainController = mainLoader.getController();
         mainController.setDealer(dealer);
+        primaryStage.setTitle("Concesionario");
+        primaryStage.setScene(new Scene(mainRoot, 800, 600));
+        primaryStage.setResizable(false);
+        primaryStage.show();
 
-        // Cargar la ventana de visualización desde el archivo FXML
+        // Cargar la ventana de visualización del Empleado
         FXMLLoader displayLoader = new FXMLLoader(getClass().getResource("DisplayView.fxml"));
         VBox displayRoot = displayLoader.load();
-
-        // Obtener el controlador de la ventana de visualización y configurarlo como observador del concesionario
         DisplayController displayController = displayLoader.getController();
         displayController.setDealer(dealer);
-
-        // Crear una nueva ventana (Stage) para la vista del inventario y mostrarla
         Stage displayStage = new Stage();
         displayStage.setTitle("Inventario de Vehículos");
-        displayStage.setScene(new Scene(displayRoot));
+        displayStage.setScene(new Scene(displayRoot, 600, 400));
+        displayStage.setResizable(false);
         displayStage.show();
-
-        // Configurar y mostrar la ventana principal
-        primaryStage.setTitle("Concesionario");
-        primaryStage.setScene(new Scene(mainRoot));
-        primaryStage.show();
     }
-
 
     public static void main(String[] args) {
-        // Método de lanzamiento de la aplicación JavaFX
         launch(args);
     }
-
 }
