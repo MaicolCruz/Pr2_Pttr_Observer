aaa00b655a2a63c85438da3030ff6f56977a90e3
AnduellgDeveloper <151770378+AnduellgDeveloper@users.noreply.github.com>
2024-10-31 10:50:02 -0500

    Se crea un Proyecto JavaFx

---
+++ co.edu.uniquindio.consecionario/src/main/java/co/edu/uniquindio/consecionario/empresa/controller/ConsecionarioController.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.consecionario.empresa.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class ConsecionarioController {
+
+    @FXML
+    protected void handleAddComment (ActionEvent event){}
+    @FXML
+    protected void handleAddLike(ActionEvent event){}
+}
